02ff29185805539b0fdd06a5e79717fae928f306
takafumi.m <taka.youthful.426@gmail.com>
2021-09-27 16:18:58 +0900

    リファクタ_markdown_to_list_csv.py

--- opt/markdown_to_list_csv.py
+++ opt/markdown_to_list_csv.py
@@ -1,33 +1,71 @@
 import csv
 
 """
- markdown.csvからデータを読み取り、list.csvへ出力する。
- マークダウン形式で記載したテーブルに変更を加えるために
- マークダウン→プレーンに修正したかったので作成。
- 実行コマンド：python markdown_to_list_csv.py
- 出力書式： | テキスト | リンク | 
+ マークダウン→csvに変換
+ input：markdown.csv
+ output：list.csv
+ command：python markdown_to_list_csv.py
+ 出力書式：
+  | 1 | [タイトルA](リンクA) | [タイトルB](リンクB) |
+  ↓
+  タイトルA,リンクA,タイトルB,リンクB
 """
 def main():
-    data_file = 'markdown.csv'
-    output_file = 'list.csv'
+    """
+    タイトルを取得
+    @param string data
+    @return string
+    """
+    def getTitle(data):
+        return data.split('[')[1].split(']')[0]
+
+    """
+    リンクを取得
+    @param string data
+    @return string
+    """
+    def getLink(data):
+        return data.split('(')[1].split(')')[0]
+
+    OUTPUT_PATH = './data/'
+    DATA_FILE = OUTPUT_PATH + 'markdown.csv'
+    OUTPUT_FILE = OUTPUT_PATH + 'list.csv'
     output_list = []
 
     # csvファイル読み取り
-    with open(data_file, 'r') as f:
+    with open(DATA_FILE, 'r') as f:
         reader = csv.reader(f)
 
         # 空白削除
-        reader =list(filter(None, reader))
+        reader = list(filter(None, reader))
 
         for index, line in enumerate(reader):
-            # タイトル
-            s1 = line[0].split('[')[1].split(']')[0]
-            # リンクURL
-            s2 = line[0].split('(')[1].split(')')[0]
-            output_list.append([s1, s2])
+            # テーブル列が3列の場合
+            if(line[0].count('|') > 3):
+                # | 1 | [タイトルA](リンクA) | [タイトルB](リンクB) |
+                if('ー' not in line[0]):
+                    source = line[0].split('|')[2]
+                    testCode = line[0].split('|')[3]
 
+                    s1 = getTitle(source)
+                    s2 = getLink(source)
+                    s3 = getTitle(testCode)
+                    s4 = getLink(testCode)
+                    output_list.append([s1, s2, s3, s4])
+                # | 1 | [タイトルA](リンクA) | ー |
+                else:
+                    source = line[0].split('|')[2]
+                    s1 = getTitle(source)
+                    s2 = getLink(source)
+                    output_list.append([s1, s2, '', ''])
+            # テーブル列が2列の場合
+            #  | 1 | [タイトルA](リンクA) |
+            else:
+                s1 = getTitle(line[0])
+                s2 = getLink(line[0])
+                output_list.append([s1, s2])
     # csvファイルへ出力
-    with open(output_file, 'w', newline='') as f2:
+    with open(OUTPUT_FILE, 'w', newline='') as f2:
         for line in output_list:
             writer = csv.writer(f2)
             writer.writerow(line)
