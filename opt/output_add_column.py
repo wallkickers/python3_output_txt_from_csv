62f6b51d61af729a0fc5353b268bf4dc4b5d20d6
takafumi.m <taka.youthful.426@gmail.com>
2021-09-27 17:14:41 +0900

    リファクタ_output_add_column.py_dataディレクトリ対応

--- opt/output_add_column.py
+++ opt/output_add_column.py
@@ -1,22 +1,29 @@
 import csv
 
 """
- list.csvからデータを読み取り、output.txtへ出力する。
- list.csvに3列目が存在する場合に列を追加する。
+ csv → マークダウン形式の表における行 に変換
+ input：list.csv
+ output：output.txt
  実行コマンド：python output_add_column.py
  出力書式：
- 　パターンA 3列目がない場合
- 　| index(1~) | [テキスト](リンク) | ー |
- 　パターンB 3列目がある場合
- 　| index(1~) | [テキスト](リンク) | [テキスト](リンク) |
+  ■パターンA 3列目がない場合
+   テキスト,リンク
+   ↓
+   | index(1~) | [テキスト](リンク) | ー |
+
+ ■パターンB 3列目がある場合
+   テキストA,リンクA,テキストB,リンクB
+   ↓
+   | index(1~) | [テキスト](リンク) | [テキスト](リンク) |
 """
 def main():
-    data_file = 'list.csv'
-    output_file = 'output.txt'
+    OUTPUT_PATH = './data/'
+    DATA_FILE = OUTPUT_PATH + 'list.csv'
+    OUTPUT_FILE = OUTPUT_PATH + 'output.txt'
     output_list = []
 
     # csvファイル読み取り
-    with open(data_file, 'r') as f:
+    with open(DATA_FILE, 'r') as f:
         reader = csv.reader(f)
 
         for index, line in enumerate(reader):
@@ -27,7 +34,7 @@ def main():
             output_list.append(s)
 
     # txtファイルへ出力
-    with open(output_file, 'w') as f2:
+    with open(OUTPUT_FILE, 'w') as f2:
         for line in output_list:
             f2.write(line)
 
